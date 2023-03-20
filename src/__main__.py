8982cd60dfa38487786ffd1ca3dbb76b03f216b4
TrpFrog <dev@trpfrog.net>
2023-03-20 23:14:22 +0900

    Migrate set_seed from transformers to ours

--- src/__main__.py
+++ src/__main__.py
@@ -1,5 +1,3 @@
-import transformers
-
 from src.config import Config
 from src.eval import evaluate
 from src.train import train
@@ -8,7 +6,7 @@ from src import utils
 
 if __name__ == '__main__':
     config = Config.from_args()
-    transformers.set_seed(config.seed)
+    utils.set_seed(config.seed)
 
     print(f'Using device: {utils.get_device()}')
 
--- src/utils.py
+++ src/utils.py
@@ -57,3 +57,14 @@ def split_to_batches(*inputs_list, batch_size: int, dim=0):
             raise TypeError(f"Unsupported type: {type(inputs)}")
 
     return [f(x) for x in inputs_list]
+
+
+def set_seed(seed: int):
+    import random
+    import numpy as np
+    import torch
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
